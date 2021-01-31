fac741e3800d21b1f40b5fadc54d2b91ce86e4bb
Resinchen <alex1410alex@gmail.com>
2021-01-31 17:58:04 +0500

    builder

--- src/@types/builder.ts
+++ src/@types/builder.ts
@@ -1,40 +1,11 @@
-import { ActionType as AT } from '../gameObjects/action'
-import { Variant } from '../gameObjects/actions/choice'
-import Jump from '../gameObjects/actions/jump'
-import Phrase, { Options } from '../gameObjects/actions/phrase'
-import { PlayType as PT } from '../gameObjects/actions/play'
-import { SetType as ST } from '../gameObjects/actions/set'
-import { Condition } from '../gameObjects/actions/varle'
-
-export type SetAction = { type: AT; setType: ST; payload?: string }
-export type PlayAction = { type: AT; playType: PT; payload: string }
-export type PhraseAction = {
-  type: AT
-  speaker: string
-  text: string
-  options?: Options
-}
-export type ChoiceAction = { type: AT; question: Phrase; variants: Variant[] }
-export type VarleAction = { type: AT; condition: Condition; target: Jump }
-export type JumpAction = { type: AT; markName: string }
-export type MarkAction = { type: AT; markName: string }
-export type LoadsceneAction = { type: AT; pathScene: string }
+import Action from '../gameObjects/action'
 
 export type CharacterLinkType = { name: string; label: string }
 export type FileType = { type: string; path: string; name: string }
-export type ActionType =
-  | SetAction
-  | PlayAction
-  | PhraseAction
-  | ChoiceAction
-  | VarleAction
-  | JumpAction
-  | MarkAction
-  | LoadsceneAction
 
 export type LoadItem = CharacterLinkType | FileType | null
 export type Scene = {
   chars: CharacterLinkType[]
   files: FileType[]
-  actions: ActionType[]
+  actions: Action[]
 }
--- src/builder.ts
+++ src/builder.ts
@@ -1,8 +1,18 @@
 import { CharacterLinkType, FileType, LoadItem, Scene } from './@types/builder'
+import { ActionType } from './gameObjects/action'
+import Play from './gameObjects/actions/play'
+import Set from './gameObjects/actions/set'
 
 export default class Builder {
   private static loadFilter = <T extends LoadItem>(loads: LoadItem[]): T[] =>
     loads.filter(item => !!item).map(item => item as T)
+
+  private static getLoadFiles(files: FileType[]): Map<string, string> {
+    const result = new Map([['', '']])
+    files.forEach(file => result.set(file.name, file.path))
+
+    return result
+  }
 
   public static build(parsedResult: Scene): Scene {
     const { chars, files, actions } = parsedResult
@@ -10,6 +20,18 @@ export default class Builder {
     // Filtering all null values from LoadList
     const filteredChars = Builder.loadFilter<CharacterLinkType>(chars)
     const filteredFiles = Builder.loadFilter<FileType>(files)
+
+    // Change path-label in setter and player to real path
+    const loadFiles = Builder.getLoadFiles(filteredFiles)
+
+    actions.forEach(action => {
+      if (action.type === ActionType.Set) {
+        ;(action as Set).payload = loadFiles.get((action as Set).payload || '')
+      }
+      if (action.type === ActionType.Play) {
+        ;(action as Play).payload = loadFiles.get((action as Play).payload)!
+      }
+    })
 
     return {
       chars: filteredChars,
