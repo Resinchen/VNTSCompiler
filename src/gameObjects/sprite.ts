5eb89414e5640c0ef1b87552676516f4f30e097d
Resinchen <alex1410alex@gmail.com>
2021-02-01 16:42:31 +0500

    fix bug: sprite field lost first letter

--- src/gameObjects/sprite.ts
+++ src/gameObjects/sprite.ts
@@ -1,5 +1,5 @@
 export default class Sprite {
   constructor(readonly field: string, readonly path: string) {
-    this.field = this.field.substring(1)
+    this.path = path.replace(/\"/g, '')
   }
 }
