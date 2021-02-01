3fb8369a8b5e2813d4098f387364157f5b0554db
Resinchen <alex1410alex@gmail.com>
2021-02-01 16:58:05 +0500

    clear character class

--- src/gameObjects/character.ts
+++ src/gameObjects/character.ts
@@ -1,21 +1,5 @@
 import Sprite from './sprite'
 
 export default class Character {
-  readonly sprites?: Map<string, Sprite>
-  //temp
-  readonly spritesJSON: Sprite[] | undefined
-
-  constructor(readonly name: string, sprites?: Sprite[]) {
-    if (sprites) {
-      this.sprites = this.buildSpriteMap(sprites)
-      //temp
-      this.spritesJSON = sprites
-    }
-  }
-
-  private buildSpriteMap = (sprites: Sprite[]): Map<string, Sprite> => {
-    const spriteMap = new Map<string, Sprite>()
-    sprites.forEach(sprite => spriteMap.set(sprite.field, sprite))
-    return spriteMap
-  }
+  constructor(readonly name: string, readonly sprites?: Sprite[]) {}
 }
